f879ffcaf20220ce458faf52c9c7bc47852801da
andrew.boren <andrew.boren@gmail.com>
2012-05-27 00:24:06 -0700

    vivow: Inherit gsm.mk, bump back to p1, remove BUILD_ID
    Change-Id: I5ea988332156e8f51fc93d129e0cd9bae80128f2

--- ev.mk
+++ ev.mk
@@ -3,6 +3,7 @@ $(call inherit-product, device/htc/vivow/full_vivow.mk)
 
 # Inherit some common evervolv stuff.
 $(call inherit-product, vendor/ev/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 #
 # Setup device specific product configuration.
@@ -12,16 +13,13 @@ PRODUCT_BRAND := verizon_wwe
 PRODUCT_DEVICE := vivow
 PRODUCT_MODEL := ADR6350
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_ID=GRI40 BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.3/GRI40/81006:user/release-keys PRIVATE_BUILD_DESC="2.18.605.4 CL81006 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.3/GRI40/81006:user/release-keys PRIVATE_BUILD_DESC="2.18.605.4 CL81006 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := Conor
-PRODUCT_VERSION_DEVICE_SPECIFIC := p2
+PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Droid Incredible 2\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
-
-#PRODUCT_PACKAGES += \
-#    Camera
 
 # Copy compatible prebuilt files
 PRODUCT_COPY_FILES +=  \
