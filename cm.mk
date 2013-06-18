a8562a15400f0f56703010e48d938246ccf14578
Spencer Hakim <spencerhakim@gmail.com>
2013-06-18 16:45:00 -0400

    Changed some file paths from ev to cm

--- cm.mk
+++ cm.mk
@@ -2,32 +2,21 @@
 $(call inherit-product, device/htc/vivow/full_vivow.mk)
 
 # Inherit some common evervolv stuff.
-$(call inherit-product, vendor/ev/config/common_full_phone.mk)
-$(call inherit-product, vendor/ev/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/cdma.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_NAME := ev_vivow
+PRODUCT_NAME := cm_vivow
 PRODUCT_BRAND := verizon_wwe
 PRODUCT_DEVICE := vivow
 PRODUCT_MODEL := ADR6350
 PRODUCT_MANUFACTURER := HTC
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.3/GRI40/81006:user/release-keys PRIVATE_BUILD_DESC="2.18.605.4 CL81006 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vivow BUILD_ID=IML74K BUILD_DISPLAY_ID=GRJ90 BUILD_FINGERPRINT=verizon_wwe/htc_vivow/vivow:2.3.4/GRJ22/120826.3:user/release-keys PRIVATE_BUILD_DESC="4.08.605.3 CL120826 release-keys"
 
-# Set up the product codename, build version & MOTD.
-PRODUCT_CODENAME := Conor
-PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Droid Incredible 2\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
-
-# Copy compatible prebuilt files
-PRODUCT_COPY_FILES +=  \
-    vendor/ev/prebuilt/wvga/media/bootanimation.zip:system/media/bootanimation.zip
-
-# Hot reboot
-PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/ev/overlay/hot_reboot
 
 # USB
 #ADDITIONAL_DEFAULT_PROPERTIES += persist.sys.usb.config=mass_storage,adb
